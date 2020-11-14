038af1feb2f7bd6f37b36a34d4adf0516193d9b3
Jens Claes <jens@datacamp.com>
2020-11-14 10:07:17 +0100

    feat: Add orderResource schemaDescriptor

--- bins/terraform-provider-hashicups/src/orderResource.ts
+++ bins/terraform-provider-hashicups/src/orderResource.ts
@@ -2,16 +2,72 @@ import {
   AsyncResponse,
   createResource,
   createSchemaDescriptor,
+  ctyNumber,
+  ctyString,
 } from '@terraform-typescript/terraform-provider';
 
 import { HashicupsApiClient } from './apiClient';
 
-export const orderResourceSchemaDescriptor = createSchemaDescriptor({
+const schemaDescriptor = createSchemaDescriptor({
   description: 'Order resource',
-  properties: {},
+  properties: {
+    items: {
+      itemType: {
+        description: 'the items in the order',
+        properties: {
+          coffee: {
+            itemType: {
+              description: 'the coffee you want to order',
+              properties: {
+                description: {
+                  ctyType: ctyString,
+                  source: 'computed',
+                  type: 'raw',
+                },
+                id: {
+                  ctyType: ctyNumber,
+                  source: 'required-in-config',
+                  type: 'raw',
+                },
+                image: {
+                  ctyType: ctyString,
+                  source: 'computed',
+                  type: 'raw',
+                },
+                name: {
+                  ctyType: ctyString,
+                  source: 'computed',
+                  type: 'raw',
+                },
+                price: {
+                  ctyType: ctyNumber,
+                  source: 'computed',
+                  type: 'raw',
+                },
+                teaser: {
+                  ctyType: ctyString,
+                  source: 'computed',
+                  type: 'raw',
+                },
+              },
+            },
+            maxItems: 1,
+            type: 'list',
+          },
+          quantity: {
+            ctyType: ctyNumber,
+            source: 'required-in-config',
+            type: 'raw',
+          },
+        },
+      },
+      type: 'list',
+    },
+  },
 });
 
-const ctor = createResource(orderResourceSchemaDescriptor);
+const ctor = createResource(schemaDescriptor);
+
 export const orderResource = ctor<HashicupsApiClient>({
   applyChange() {
     return AsyncResponse.left([]);
