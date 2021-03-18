bb01d64b746515309328a92bd6296563587341a3
Jens Claes <jens@datacamp.com>
2021-03-18 08:34:07 +0100

    finish Attribute

--- libs/terraform-provider-sdk/src/schema/descriptor.ts
+++ libs/terraform-provider-sdk/src/schema/descriptor.ts
@@ -1,6 +1,17 @@
 import { AsyncResponse } from '../types/response';
 
-import { ctyString, CtyType } from './ctyType';
+import {
+  ctyAny,
+  ctyBoolean,
+  ctyList,
+  ctyMap,
+  ctyNumber,
+  ctyObject,
+  ctySet,
+  ctyString,
+  ctyTuple,
+  CtyType,
+} from './ctyType';
 import type { AttributePropertyConfigBySource } from './SchemaConfig';
 
 // This const is exported in this file but not in the global package to ensure typescript
@@ -92,10 +103,71 @@ export const validatedAttribute = <
   };
 };
 export const Attribute = {
+  computed: {
+    any: validatedAttribute('computed', ctyAny),
+    boolean: validatedAttribute('computed', ctyBoolean),
+    list: <C extends CtyType>(itemType: C) =>
+      validatedAttribute('computed', ctyList(itemType)),
+    map: <C extends CtyType>(itemType: C) =>
+      validatedAttribute('computed', ctyMap(itemType)),
+    number: validatedAttribute('computed', ctyNumber),
+    object: <R extends Record<string, CtyType>>(itemTypes: R) =>
+      validatedAttribute('computed', ctyObject(itemTypes)),
+    set: <C extends CtyType>(itemType: C) =>
+      validatedAttribute('computed', ctySet(itemType)),
+    string: validatedAttribute('computed', ctyString),
+    tuple: <T extends CtyType[]>(...itemTypes: T) =>
+      validatedAttribute('computed', ctyTuple(...itemTypes)),
+  },
+  computedButOverridable: {
+    any: validatedAttribute('computed-but-overridable', ctyAny),
+    boolean: validatedAttribute('computed-but-overridable', ctyBoolean),
+    list: <C extends CtyType>(itemType: C) =>
+      validatedAttribute('computed-but-overridable', ctyList(itemType)),
+    map: <C extends CtyType>(itemType: C) =>
+      validatedAttribute('computed-but-overridable', ctyMap(itemType)),
+    number: validatedAttribute('computed-but-overridable', ctyNumber),
+    object: <R extends Record<string, CtyType>>(itemTypes: R) =>
+      validatedAttribute('computed-but-overridable', ctyObject(itemTypes)),
+    set: <C extends CtyType>(itemType: C) =>
+      validatedAttribute('computed-but-overridable', ctySet(itemType)),
+    string: validatedAttribute('computed-but-overridable', ctyString),
+    tuple: <T extends CtyType[]>(...itemTypes: T) =>
+      validatedAttribute('computed-but-overridable', ctyTuple(...itemTypes)),
+  },
   optional: {
+    any: validatedAttribute('optional-in-config', ctyAny),
+    boolean: validatedAttribute('optional-in-config', ctyBoolean),
+    list: <C extends CtyType>(itemType: C) =>
+      validatedAttribute('optional-in-config', ctyList(itemType)),
+    map: <C extends CtyType>(itemType: C) =>
+      validatedAttribute('optional-in-config', ctyMap(itemType)),
+    number: validatedAttribute('optional-in-config', ctyNumber),
+    object: <R extends Record<string, CtyType>>(itemTypes: R) =>
+      validatedAttribute('optional-in-config', ctyObject(itemTypes)),
+    set: <C extends CtyType>(itemType: C) =>
+      validatedAttribute('optional-in-config', ctySet(itemType)),
     string: validatedAttribute('optional-in-config', ctyString),
-  } as const,
-} as const;
+    tuple: <T extends CtyType[]>(...itemTypes: T) =>
+      validatedAttribute('optional-in-config', ctyTuple(...itemTypes)),
+  },
+  required: {
+    any: validatedAttribute('required-in-config', ctyAny),
+    boolean: validatedAttribute('required-in-config', ctyBoolean),
+    list: <C extends CtyType>(itemType: C) =>
+      validatedAttribute('required-in-config', ctyList(itemType)),
+    map: <C extends CtyType>(itemType: C) =>
+      validatedAttribute('required-in-config', ctyMap(itemType)),
+    number: validatedAttribute('required-in-config', ctyNumber),
+    object: <R extends Record<string, CtyType>>(itemTypes: R) =>
+      validatedAttribute('required-in-config', ctyObject(itemTypes)),
+    set: <C extends CtyType>(itemType: C) =>
+      validatedAttribute('required-in-config', ctySet(itemType)),
+    string: validatedAttribute('required-in-config', ctyString),
+    tuple: <T extends CtyType[]>(...itemTypes: T) =>
+      validatedAttribute('required-in-config', ctyTuple(...itemTypes)),
+  },
+};
 
 export type ListPropertyDescriptor = SchemaPropertyDescriptor & {
   type: 'list';
